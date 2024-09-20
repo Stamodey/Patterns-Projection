712033489bcc2727cd81732e15bed9e502582cf4
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-09-21 01:48:35 +0300

    create_class_student_for_task_2

---
+++ reading_viewing_and_recording_entities/student.rb
@@ -0,0 +1,36 @@
+class Student
+  attr_reader :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :git
+
+  # Основной конструктор с параметрами
+  def initialize(id, last_name, first_name, middle_name, phone = nil, telegram = nil, email = nil, git = nil)
+    self.id = id
+    self.last_name = last_name
+    self.first_name = first_name
+    self.middle_name = middle_name
+    self.phone = phone
+    self.telegram = telegram
+    self.email = email
+    self.git = git
+  end
+
+  # Конструктор, принимающий строку
+  def self.from_string(str)
+    data = str.split(', ')
+    raise 'Invalid format' if data.size < 4 || data.size > 8
+
+    # Разделение обязательных и необязательных данных
+    id = data[0].to_i
+    last_name = data[1]
+    first_name = data[2]
+    middle_name = data[3]
+    phone = data[4] if data.size > 4
+    telegram = data[5] if data.size > 5
+    email = data[6] if data.size > 6
+    git = data[7] if data.size > 7
+
+    # Вызов стандартного конструктора
+    new(id, last_name, first_name, middle_name, phone, telegram, email, git)
+  rescue => e
+    raise "Error parsing string: #{e.message}"
+  end
+end
