5a6f4c9b11195e1dc127ed2ea58ea4d7bb4a3a05
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-09-21 01:11:11 +0300

    valid_git_email_telegram_in_main

--- objects_and_classes/main.rb
+++ objects_and_classes/main.rb
@@ -1,8 +1,8 @@
 require_relative 'student'
 
 # Создание студентов с различными комбинациями полей
-student1 = Student.new(last_name: "Иванов", first_name: "Иван", middle_name: "Иванович", phone: "+79991234567", git: "github.com/ivanov")
-student2 = Student.new(last_name: "Петров", first_name: "Петр", middle_name: "Петрович", email: "petrov@mail.com", git: "github.com/petrov")
+student1 = Student.new(last_name: "Иванов", first_name: "Иван", middle_name: "Иванович", phone: "+79991234567", git: "https://github.com/ivanov")
+student2 = Student.new(last_name: "Петров", first_name: "Петр", middle_name: "Петрович", email: "petrov@mail.com", git: "https://github.com/petrov/repo.git")
 
 # Вывод информации о студентах
 puts student1
