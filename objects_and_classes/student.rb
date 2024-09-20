0935ad92277b88ea9dbcc27c9edc2340d1ba0479
Stamodey <cobexiadanil@gmail.com>
2024-09-20 17:14:04 +0300

    implemented_the_Student_class_and_output_information_about_students
    Created a Student class with the necessary attributes and methods. Student objects were created and their information was displayed.

--- objects_and_classes/student.rb
+++ objects_and_classes/student.rb
@@ -1,7 +1,10 @@
 class Student
+  # делаем атрибуты доступными через методы getter и setter
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :git
 
-  def initialize(id, last_name, first_name, middle_name = nil, phone = nil, telegram = nil, email = nil, git = nil)
+  # Инициализировать новый объект Student с заданными атрибутами
+  def initialize(id, last_name, first_name, middle_name, phone = nil, telegram = nil, email = nil, git = nil)
+    # Установить атрибуты используя предоставленные значения
     self.id = id
     self.last_name = last_name
     self.first_name = first_name
@@ -12,12 +15,9 @@ class Student
     self.git = git
   end
 
+  # возвращаем строковое представление объекта Student
   def to_s
+    # Форматировать строку для включения всех атрибутов
     "ID: #{id}, Фамилия: #{last_name}, Имя: #{first_name}, Отчество: #{middle_name}, Телефон: #{phone}, Telegram: #{telegram}, Email: #{email}, Git: #{git}"
-  end
-
-  def validate!
-    raise ArgumentError, 'Git не может быть пустым' if git.nil? || git.empty?
-    raise ArgumentError, 'Необходимо указать хотя бы один контакт' if phone.nil? && telegram.nil? && email.nil?
   end
 end
