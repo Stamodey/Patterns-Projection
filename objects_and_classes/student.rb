bfd44578f8223491ce7319efe66da5eb8e51f8d6
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-09-20 19:03:07 +0300

    initializing_a_student_class_with_support_for_required_and_optional_attributes

--- objects_and_classes/student.rb
+++ objects_and_classes/student.rb
@@ -1,23 +1,28 @@
 class Student
-  # делаем атрибуты доступными через методы getter и setter
+  # Делаем атрибуты доступными через методы getter и setter
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :git
 
-  # Инициализировать новый объект Student с заданными атрибутами
-  def initialize(id, last_name, first_name, middle_name, phone = nil, telegram = nil, email = nil, git = nil)
-    # Установить атрибуты используя предоставленные значения
-    self.id = id
-    self.last_name = last_name
-    self.first_name = first_name
-    self.middle_name = middle_name
-    self.phone = phone
-    self.telegram = telegram
-    self.email = email
-    self.git = git
+  # Инициализируем новый объект Student с заданными атрибутами
+  def initialize(attributes = {})
+    self.id = attributes[:id]                   # Устанавливаем ID
+    self.last_name = attributes[:last_name]     # Устанавливаем Фамилию
+    self.first_name = attributes[:first_name]   # Устанавливаем Имя
+    self.middle_name = attributes[:middle_name] # Устанавливаем Отчество
+    set_optional_attributes(attributes)         # Устанавливаем необязательные атрибуты
   end
 
-  # возвращаем строковое представление объекта Student
+  private
+
+  # Метод для установки необязательных атрибутов
+  def set_optional_attributes(attributes = {})
+    self.phone = attributes[:phone] if attributes[:phone]             # Устанавливаем Телефон, если он есть
+    self.telegram = attributes[:telegram] if attributes[:telegram]     # Устанавливаем Telegram, если он есть
+    self.email = attributes[:email] if attributes[:email]               # Устанавливаем Email, если он есть
+    self.git = attributes[:git] if attributes[:git]                     # Устанавливаем Git, если он есть
+  end
+
+  # Возвращаем строковое представление объекта Student
   def to_s
-    # Форматировать строку для включения всех атрибутов
     "ID: #{id}, Фамилия: #{last_name}, Имя: #{first_name}, Отчество: #{middle_name}, Телефон: #{phone}, Telegram: #{telegram}, Email: #{email}, Git: #{git}"
   end
 end
