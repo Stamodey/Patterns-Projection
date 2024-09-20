97e23cdddc989aaa4b4e3c51e94f4b897551a3f2
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-09-20 16:39:17 +0300

    Update student.rb

--- objects_and_classes/student.rb
+++ objects_and_classes/student.rb
@@ -1,14 +1,14 @@
 class Student
 	attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :git
-
+	
 	def initialize(id, last_name, first_name, middle_name, phone = nil, telegram = nil, email = nil, git = nil)
 		self.id = id
-    	self.last_name = last_name
-    	self.first_name = first_name
-    	self.middle_name = middle_name
-    	self.phone = phone
-    	self.telegram = telegram
-    	self.email = email
-    	self.git = git
+    		self.last_name = last_name
+    		self.first_name = first_name
+    		self.middle_name = middle_name
+    		self.phone = phone
+    		self.telegram = telegram
+    		self.email = email
+    		self.git = git
 	end
 end
