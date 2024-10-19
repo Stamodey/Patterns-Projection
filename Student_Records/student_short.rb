9c091fe29f1f81739b319c9f69387ce8fef10a24
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-10-19 06:19:14 +0300

    Update student_short.rb

--- Student_Records/student_short.rb
+++ Student_Records/student_short.rb
@@ -1,31 +1,52 @@
-require_relative 'student'
+# student_short.rb
+require_relative 'person'
 
-# Класс Student_short для создания объекта с укороченной информацией о студенте
-class Student_short < Person
-  # Конструктор для инициализации через объект Student
-  def initialize(student)
-    # Вызов конструктора суперкласса с нужными полями
-    super(student.id, student.last_name, student.first_name, student.middle_name, student.git, student.contact_info)
+class StudentShort < Person
+  attr_reader :initials, :contacts
+
+  # Конструктор объекта класса StudentShort
+  def initialize(data_or_id)
+    case data_or_id
+    when Student
+      initialize_from_student(data_or_id)
+    when String
+      initialize_from_string(data_or_id)
+    else
+      raise ArgumentError, 'Invalid argument type. Must be Student or String.'
+    end
   end
 
-  # Альтернативный конструктор: принимает ID и строку с информацией
-  def self.from_string(id, info_str)
-    # Разделение строки по запятым
-    data = info_str.split(', ')
-    raise 'Invalid format' if data.size < 3
+  # Метод для отображения информации
+  def to_s
+    info = []
+    info.push("ID: #{@id}") if @id
+    info.push("Initials: #{@initials}")
+    info.push("GitHub: #{@git}") if @git
+    info.push("#{@contacts.join("; ")}") unless @contacts.empty?
+    info.compact.join("; ")
+  end
 
-    # Присвоение переменных
-    last_name = data[0]
-    first_name = data[1]
-    middle_name = data[2]
-    git = data[3] if data.size > 3
+  private
+
+  # Приватные методы инициализации
+  def initialize_from_student(student)
+    @id = student.id
+    @initials = "#{student.last_name} #{student.first_name[0]}. #{student.middle_name[0]}."
+    @git = student.git
+    student.contacts.each { |type, value| add_contact(value, type.capitalize) }
+  end
+
+  def initialize_from_string(string)
+    data = string.split(', ')
+    @id = data[0]
+    @git = data[3]
     contact = data[4] if data.size > 4
+    add_contact(contact, 'Contact') if contact
+  end
 
-    # Создаем временный объект Student и изменяем контактную информацию
-    student = Student.new(id, last_name, first_name, middle_name, nil, nil, nil, git)
-    student.instance_variable_set(:@contact_info, contact)
-
-    # Возвращаем объект Student_short на основе Student
-    new(student)
+  # Метод для добавления контакта
+  def add_contact(value, type)
+    @contacts ||= []  # Инициализация массива контактов
+    @contacts << "#{type}: #{value}" if value
   end
 end
