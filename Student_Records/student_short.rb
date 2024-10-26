6bdd0b2d711409f90c9534a80236ab4bdc0c2b20
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-10-26 12:46:16 +0300

    lab_2_student_short.rb

--- Student_Records/student_short.rb
+++ Student_Records/student_short.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class StudentShort < Person
-  attr_reader :initials, :contacts
+  attr_reader :initials
 
   # Конструктор объекта класса StudentShort
   def initialize(data_or_id)
@@ -21,9 +21,6 @@ class StudentShort < Person
     info.push("ID: #{@id}") if @id
     info.push("Initials: #{@initials}")
     info.push("GitHub: #{@git}") if @git
-    if @contacts.any?
-      info.push("Contact: #{@contacts.first}")
-    end
     info.compact.join("; ")
   end
 
@@ -34,20 +31,11 @@ class StudentShort < Person
     @id = student.id
     @initials = "#{student.last_name} #{student.first_name[0]}. #{student.middle_name[0]}."
     @git = student.git
-    student.contacts.each { |type, value| add_contact(value, type.capitalize) }
   end
 
   def initialize_from_string(string)
     data = string.split(', ')
     @id = data[0]
     @git = data[3]
-    contact = data[4] if data.size > 4
-    add_contact(contact, 'Contact') if contact
-  end
-
-  # Метод для добавления контакта
-  def add_contact(value, type)
-    @contacts ||= [] # Инициализация массива контактов
-    @contacts << "#{type}: #{value}" if value
   end
 end
