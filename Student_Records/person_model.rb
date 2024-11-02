ad3db8a954598a22f98ae3a488ee2b03d1973d01
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-11-02 11:10:58 +0300

    lab_2_person_model

--- Student_Records/person_model.rb
+++ Student_Records/person_model.rb
@@ -1,54 +1,57 @@
 class Person
-  
   attr_reader :last_name, :first_name, :middle_name, :git, :id
 
-  # Конструктор объекта класса Person
   def initialize(last_name:, first_name:, middle_name:, id: nil, git: nil)
     self.id = id
-    self.last_name = last_name  # Сеттер для фамилии
-    self.first_name = first_name # Сеттер для имени
-    self.middle_name = middle_name # Сеттер для отчества
-    self.git = git               # Сеттер для Git
+    self.last_name = last_name
+    self.first_name = first_name
+    self.middle_name = middle_name
+    self.git = git
   end
 
-  # Сеттеры для атрибутов
+  protected
+
   def last_name=(last_name)
-    validate_name!(last_name, 'Фамилия должна содержать только буквы')
-    @last_name = last_name
+    if self.class.valid_name?(last_name)
+      @last_name = last_name
+    else
+      false
+    end
   end
 
   def first_name=(first_name)
-    validate_name!(first_name, 'Имя должно содержать только буквы')
-    @first_name = first_name
+    if self.class.valid_name?(first_name)
+      @first_name = first_name
+    else
+      false
+    end
   end
 
   def middle_name=(middle_name)
-    validate_name!(middle_name, 'Отчество должно содержать только буквы')
-    @middle_name = middle_name
+    if self.class.valid_name?(middle_name)
+      @middle_name = middle_name
+    else
+      false
+    end
   end
 
   def git=(git)
-    raise 'Неверный формат Git' unless git.nil? || self.class.valid_git?(git)
-    @git = git
+    if git.nil? || self.class.valid_git?(git)
+      @git = git
+    else
+      false
+    end
   end
-  
+
   def id=(id)
     @id = id
   end
 
-  # Методы класса для валидации
   def self.valid_git?(git)
     git.match?(/\Ahttps:\/\/github\.com\/[a-zA-Z0-9._-]+\z/)
   end
 
   def self.valid_name?(name)
     name.match?(/\A[а-яА-Яa-zA-Z]+\z/)
-  end
-
-  private
-
-  # Приватный метод для валидации имени
-  def validate_name!(name, error_message)
-    raise error_message unless self.class.valid_name?(name)
   end
 end
