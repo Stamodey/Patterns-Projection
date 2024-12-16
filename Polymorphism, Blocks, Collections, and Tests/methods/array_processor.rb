3df6da4915f9650930073bf86bc82088f569fb8b
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-12-16 05:15:47 +0300

    lab_3_task_2

--- Polymorphism, Blocks, Collections, and Tests/methods/array_processor.rb
+++ Polymorphism, Blocks, Collections, and Tests/methods/array_processor.rb
@@ -1,66 +1,67 @@
+# Реализация класса ArrayProcessor
 class ArrayProcessor
   def initialize(array)
     @array = array.dup.freeze
   end
 
+  # Метод для получения элементов массива
   def elements
     @array
   end
 
+  # Реализация метода `all?`
   def all?
-    @array.each do |element|
-      return false unless yield(element)
-    end
+    return false unless block_given?
+    @array.each { |element| return false unless yield(element) }
     true
   end
 
+  # Реализация метода `find`
   def find
-    @array.each do |element|
-      return element if yield(element)
-    end
+    return nil unless block_given?
+    @array.each { |element| return element if yield(element) }
     nil
   end
 
+  # Реализация метода `flat_map`
   def flat_map
+    return [] unless block_given?
     result = []
-    @array.each do |element|
-      result.concat(yield(element))
-    end
+    @array.each { |element| result.concat(yield(element)) }
     result
   end
 
+  # Реализация метода `min_by`
   def min_by
-    return nil if @array.empty?
-
+    return nil unless block_given?
     min_element = @array.first
     min_value = yield(min_element)
-
     @array.each do |element|
-      value = yield(element)
-      if value < min_value
+      current_value = yield(element)
+      if current_value < min_value
         min_element = element
-        min_value = value
+        min_value = current_value
       end
     end
-
     min_element
   end
 
+  # Реализация метода `one?`
   def one?
-    found = false
+    return false unless block_given?
+    count = 0
     @array.each do |element|
-      if yield(element)
-        return false if found
-        found = true
-      end
+      count += 1 if yield(element)
+      return false if count > 1
     end
-    found
+    count == 1
   end
 
-  def inject(initial)
+  # Реализация метода `inject`
+  def inject(initial = nil)
     accumulator = initial
     @array.each do |element|
-      accumulator = yield(accumulator, element)
+      accumulator = accumulator.nil? ? element : yield(accumulator, element)
     end
     accumulator
   end
