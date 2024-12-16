a9d5b456add72791c6922116b3771d37c8f88dba
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-12-16 22:56:44 +0300

    lab_2_main

--- Student_Records/main.rb
+++ Student_Records/main.rb
@@ -1,26 +1,81 @@
+require_relative 'person'
 require_relative 'student'
-require_relative 'file_operations'
 require_relative 'student_short'
 
-begin
-  student_str = '1, Иванов, Иван, Иванович, +79001234567, @ivanov, ivanov@example.com, https://github.com/ivanov'
-  student = Student.from_string(student_str)
-  puts "Создан объект:\n#{student.get_info}"
+def write_students_to_file(file_path, file_name, students)
+  raise ArgumentError, "Invalid file path: #{file_path}" unless File.exist?(File.dirname(file_path))
 
-  puts "\nЧтение данных из файла 'students.txt':"
-  students_from_file = read_from_txt('D:\Ruby\app\students.txt')
-  students_from_file.each { |student| puts student.get_info }
-
-  puts "\nДобавляем нового студента в список:"
-  new_student_str = ', Петров, Петр, Петрович, +79007654321, @petrov, petrov@example.com, https://github.com/petrov'
-  new_student = Student.from_string(new_student_str)
-  students_from_file << new_student
-  write_to_txt('students.txt', students_from_file)
-  puts "Обновленный список студентов записан в 'students.txt'."
-
-  puts "\nВывод StudentShort:"
-  student_short = StudentShort.new(new_student)
-  puts student_short.to_s
-rescue => e
-  puts e.message
+  Dir.mkdir(File.dirname(file_path)) unless Dir.exist?(File.dirname(file_path))
+  File.open("#{file_path}/#{file_name}.txt", 'w') do |file|
+    students.each do |student|
+      file.puts student.to_s
+    end
+  end
 end
+
+def read_from_file(file_path)
+  raise ArgumentError, "Invalid file path: #{file_path}" unless File.exist?(file_path)
+
+  students = []
+  File.foreach(file_path) do |line|
+    students << Student.from_string(line.chomp)
+  end
+
+  students
+end
+
+# Создание объекта Student через конструктор
+student = Student.new(
+  first_name: "Иван",
+  last_name: "Иванов",
+  middle_name: "Иванович",
+  student_id: "12345",
+  git: "https://github.com/ivanov/portfolio",
+  phone: "+7 (123) 456-7890",
+  email: "ivanov@example.com",
+  telegram: "@ivanov"
+)
+
+puts "--- Студент из конструктора ---"
+puts student.to_s
+puts "Контактная информация: #{student.contact_info}"
+puts "Краткая информация: #{student.get_info}"
+puts "Есть ли контакт: #{student.contact_available? ? 'Да' : 'Нет'}"
+puts
+
+# Создание объекта StudentShort через создание из объекта Student
+student_short = StudentShort.create_from_student(student)
+
+puts "--- СтудентShort из объекта Student ---"
+puts student_short.to_s
+puts "Есть ли контакт: #{student_short.contact_available? ? 'Да' : 'Нет'}"
+puts
+
+# Создание объекта StudentShort из строки
+data = "student_id: 12345, git: https://github.com/ivanov/portfolio, surname_initials: Иванов И. И., contact: +7 (123) 456-7890"
+student_short_from_string = StudentShort.create_from_string(data)
+
+puts "--- СтудентShort из строки ---"
+puts student_short_from_string.to_s
+puts "Есть ли контакт: #{student_short_from_string.contact_available? ? 'Да' : 'Нет'}"
+
+# Создаем несколько студентов
+students = [
+  Student.from_string('ID: 1, Фамилия: Иванов, Имя: Иван, Отчество: Иванович, Телефон: +79162345678, Телеграм: @ivanov, Почта: ivanov@gmail.com, Гит: https://github.com/ivanov/portfolio'),
+  Student.from_string('ID: 2, Фамилия: Петров, Имя: Петя, Отчество: Петрович, Телефон: +98765432101, Телеграм: @petrov, Почта: petrov@gmail.com, Гит: https://github.com/ivanov/portfolio'),
+]
+
+write_students_to_file('', 'data', students)
+
+# Чтение студентов из файла и вывод информации о них
+read_from_file('data.txt').each do |student|
+  puts student.get_info
+end
+
+# Создаем экземпляры Student_short
+student1_short = StudentShort.create_from_student(students[0])
+student2_short = StudentShort.create_from_student(students[1])
+
+# Выводим информацию о студентах
+puts student1_short.to_s
+puts student2_short.to_s
