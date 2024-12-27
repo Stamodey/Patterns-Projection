3aaa2e4cd6e0d9b389c2f2490d87777ad5502b38
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-12-27 22:25:19 +0300

    lab_4.1_Data_list_student_short.rb

---
+++ Model Classes/Data_list_student_short.rb
@@ -0,0 +1,26 @@
+require_relative 'student_short'
+require_relative 'Data_list'
+
+class Data_list_student_short < Data_list
+
+  def initialize(data)
+    super(data)
+  end
+
+  def get_names
+    return ["id", "initials", "git", "contact"]
+  end
+
+  def attributes(student_short)
+    # Теперь возвращаем только необходимые данные для 4 столбцов
+    return [student_short.student_id, student_short.initials, student_short.git, student_short.contact]
+  end
+
+  def get_data
+    table = []
+    @array.each_with_index do |student_short, index|
+      table << [student_short.student_id, student_short.initials, student_short.git, student_short.contact]
+    end
+    DataTable.new(table)
+  end
+end
