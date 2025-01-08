10af3cd8b53b0ff5264773327f0a980657ddd4a8
Stamodey <75629745+Stamodey@users.noreply.github.com>
2025-01-08 12:43:48 +0300

    lab_4.2_Data_storage_staratege

---
+++ Model Classes/Data_storage_staratege.rb
@@ -0,0 +1,10 @@
+class Data_storage_staratege
+  
+  def from(file)
+    raise NotImplementedError, "Метод from не реализован"
+  end
+
+  def to(student, file)
+    raise NotImplementedError, "Метод to не реализован"
+  end
+end
